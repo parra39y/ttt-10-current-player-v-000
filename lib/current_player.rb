916f15837cfa2fde84d05d6d643c34e288e81481
Yesenia Parra <parra22y@mtholyoke.edu>
2018-07-08 18:09:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,9 +4,21 @@ def turn_count(board)
 counter = 0
 
 board.each do |index|
-  if index = "X" || index = "O"
+  if index == "X" || index == "O"
       counter += 1
   end
 end
 return counter
 end
+
+def current_player (turn_count)
+  counter = 0
+  if counter % 2 == 0
+    puts "X"
+  elsif counter % 2 == 1
+    puts "O"  
+    counter +1
+  end
+end
+    
+    
