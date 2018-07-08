1493d22e7e60bf9176ff506b53eeef42589b51fe
Yesenia Parra <parra22y@mtholyoke.edu>
2018-07-08 18:48:59 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,13 +12,18 @@ return counter
 end
 
 def current_player(board)
+  counter = 0
 
   board.each do |index|
   if index % 2 == 0
     puts "X"
-
-  elsif index % 2 == 1
+    
+  elsif 
     puts "O"
 
+    Counter += 1
+
   end
+end
+return counter
 end
